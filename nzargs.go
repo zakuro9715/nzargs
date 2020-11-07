8de7bed71a1c0d191fa33ebf348469b08b5ede92
zakuro <z@kuro.red>
2020-11-07 23:37:23 +0900

    Add comments

--- nzargs.go
+++ nzargs.go
@@ -6,19 +6,23 @@ import (
 	"strings"
 )
 
+// App holds config for normalize
 type App struct {
 	FlagsValueN map[string]int
 }
 
+// New returns new App instance
 func New() *App {
 	return &App{map[string]int{}}
 }
 
+// FlagN sets count of flag value
 func (app *App) FlagN(name string, n int) *App {
 	app.FlagsValueN[name] = n
 	return app
 }
 
+// GetFlagN returns count of flag value
 func (app *App) GetFlagN(name string) int {
 	n, ok := app.FlagsValueN[name]
 	if ok {
@@ -27,6 +31,7 @@ func (app *App) GetFlagN(name string) int {
 	return 0
 }
 
+// NormalizeArgs normalize os.Args
 func (app *App) NormalizeArgs() {
 	app.Normalize(os.Args[1:])
 }
@@ -42,6 +47,7 @@ func checkFlagValue(name string, n int, args []string) error {
 	return nil
 }
 
+// Normalize parses argv
 func (app *App) Normalize(argv []string) ([]Value, error) {
 	normalized := make([]Value, 0)
 	for i := 0; i < len(argv); i++ {
@@ -66,20 +72,20 @@ func (app *App) Normalize(argv []string) ([]Value, error) {
 			v = strings.TrimPrefix(v, "-")
 			splited := strings.SplitN(v, "=", 2)[0:2]
 			names := splited[0]
-			last_name := string(names[len(names)-1])
+			lastName := string(names[len(names)-1])
 			for _, name := range names[:len(names)-1] {
 				normalized = append(normalized, NewFlag(string(name)))
 			}
 			if len(splited[1]) == 0 {
-				n := app.GetFlagN(last_name)
-				if err := checkFlagValue(last_name, n, argv[i+1:]); err != nil {
+				n := app.GetFlagN(lastName)
+				if err := checkFlagValue(lastName, n, argv[i+1:]); err != nil {
 					return nil, err
 				}
-				normalized = append(normalized, NewFlag(last_name, argv[i+1:i+1+n]...))
+				normalized = append(normalized, NewFlag(lastName, argv[i+1:i+1+n]...))
 				i += n
 			} else {
 				values := strings.Split(splited[1], ",")
-				normalized = append(normalized, NewFlag(last_name, values...))
+				normalized = append(normalized, NewFlag(lastName, values...))
 			}
 		default:
 			normalized = append(normalized, NewArg(v))
@@ -88,6 +94,7 @@ func (app *App) Normalize(argv []string) ([]Value, error) {
 	return normalized, nil
 }
 
+// NormalizeToString normalize argv and returns result as text slice
 func (app *App) NormalizeToString(argv []string) ([]string, error) {
 	normalized, err := app.Normalize(argv)
 	if err != nil {
--- value.go
+++ value.go
@@ -5,13 +5,17 @@ import (
 	"strings"
 )
 
+// ValueType represents type of Value
 type ValueType int
 
 const (
+	// TypeFlag is flag
 	TypeFlag ValueType = iota
+	// TypeArg is arg
 	TypeArg
 )
 
+// Value represents Arg or Flag
 type Value interface {
 	Type() ValueType
 	Text() string
@@ -19,27 +23,33 @@ type Value interface {
 	Arg() *Arg
 }
 
+// Flag is cli flag
 type Flag struct {
 	Name   string
 	Values []string
 }
 
+// NewFlag returns flag instance
 func NewFlag(name string, values ...string) *Flag {
 	return &Flag{name, values}
 }
 
+// Flag returns itself
 func (v *Flag) Flag() *Flag {
 	return v
 }
 
+// Arg returns nil
 func (v *Flag) Arg() *Arg {
 	return nil
 }
 
+// Type returns TypeFlag
 func (v *Flag) Type() ValueType {
 	return TypeFlag
 }
 
+// Text returns flag as text
 func (v *Flag) Text() string {
 	name := v.Name
 	if len(name) == 1 {
@@ -53,26 +63,32 @@ func (v *Flag) Text() string {
 	return fmt.Sprintf("%v=%v", name, strings.Join(v.Values, ","))
 }
 
+// Arg represets cli argument
 type Arg struct {
 	Value string
 }
 
+// NewArg returns new Arg instance
 func NewArg(value string) *Arg {
 	return &Arg{value}
 }
 
+// Flag returns nil
 func (v *Arg) Flag() *Flag {
 	return nil
 }
 
+// Arg returns itself
 func (v *Arg) Arg() *Arg {
 	return v
 }
 
+// Type returns TypeArg
 func (v *Arg) Type() ValueType {
 	return TypeArg
 }
 
+// Text returns arg as text
 func (v *Arg) Text() string {
 	return v.Value
 }
